a2a61f19c0d08833c9392ace30d853aaa41ec7cd
jackmarley254 <engndiritu1@gmail.com>
2024-05-01 05:41:47 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
-xport default function updateStudentGradeByCity(data, city, newGrades) {
+export default function updateStudentGradeByCity(data, city, newGrades) {
   const sanFstudents = data.filter((x) => x.location === city);
   const newData = sanFstudents.map((x) => {
     const studentId = newGrades.find((entry) => entry.studentId === x.id);
